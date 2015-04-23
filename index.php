5ec7833a0d3ed5c419b24bbecac52e122a93ab57
Gregory Thomason <gregory_thomason@hotmail.com>
2015-04-22 20:18:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-		// loop through proficiencies and print to page
-		// echo '<pre>', print_r($row['subrepeater'], true), '</pre>';
-		$rows = get_field('menu_general_item_repeater');
-		foreach($rows as $row) {
-			echo $row['menu_category']->name.'<br>';
+// loop through proficiencies and print to page
+$rows = get_field('menu_general_item_repeater');
+	
+foreach($rows as $row) {
+	echo $row['menu_category']->name.'<br>';
 			
-			foreach($row['subrepeater'] as $item) {
-				echo $item['item_text'].'<br>';
-				echo $item['item_price'].'<br>';
-			}
-		
-		};
+	foreach($row['subrepeater'] as $item) {
+		echo $item['item_text'].'<br>';
+		echo $item['item_price'].'<br>';
+	}
+};
 		
